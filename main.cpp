5c4bb0d90a5617c0a04a8e81c0d5c2abc000f130
yangyang0611 <https://github.com/yangyang0611>
2021-05-13 16:03:26 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Player.h"
+#include"Game.h"
+#include <QApplication>
+#include<QGraphicsScene>
+#include<QGraphicsView>
+#include<QTimer>
+
+Game * game;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    game = new Game();
+    game->show();
+
+
+    return a.exec();
+}
